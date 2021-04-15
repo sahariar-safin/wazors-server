186d43e54f715bf50ea90dd21080d835a7dc4c43
Shafin The Developer <sahariar350@gmail.com>
2021-04-15 14:12:47 +0600

    something is cooking

--- index.js
+++ index.js
@@ -3,10 +3,44 @@ const app = express()
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+const FileUpload = require('express-fileupload');
+const imageToBase64 = require('image-to-base64');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('service'));
+app.use(FileUpload());
+
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.356j9.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const services = client.db(`${ process.env.DB_NAME }`).collection("services");
+
+    app.post('/addservice', (req, res) => {
+        const newImg = req.files.image.data;
+
+        const image = {
+            contentType: req.files.image.mimetype,
+            size: req.files.image.size,
+            img: Buffer.from(newImg).toString('base64')
+        };
+        req.body.image = image;
+        services.insertOne(req.body)
+            .then(response => {
+                console.log(response);
+                res.send(response);
+            })
+    })
+
+    app.get('/services', (req, res) => {
+        services.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
